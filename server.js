9e0a115f893aac80393ea345794523bcc38fe172
José Ferreira <ferreirajosejp@gmail.com>
2018-12-25 11:08:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.static(__dirname + '/dist/frotas-azul'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/frotas-azul/index.html'))
-})
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port {}`);
-})
+  console.log(`App listening on port ${PORT}`);
+});
